005fd30b6dab2c45f0295210cae8cd2546196839
Annoyingpanthera <122933423+Annoyingpanthera@users.noreply.github.com>
2023-09-17 13:44:41 +0200

    feat:finish pc version (Sun, Sep 17, 2023 01:44:41 PM)

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     const sliderButtons = document.querySelectorAll('.slider_button');
     const images = document.querySelectorAll('.img');
+    const titleSlider = document.querySelector('.title_slider span');
+
     let currentIndex = 0;
 
     function showImage(index) {
-        // Скрываем все изображения, кроме активного
+        // Скрываем все изображения и текст, кроме активных
         images.forEach((img, i) => {
             if (i !== index) {
                 img.classList.remove('active');
             }
         });
 
-        // Показываем только выбранное изображение
+        // Показываем только выбранное изображение и текст
         images[index].classList.add('active');
+        titleSlider.textContent = images[index].querySelector('.caption').textContent;
+        titleSlider.classList.remove('text-animation'); // Убираем класс анимации
+        void titleSlider.offsetWidth; // Принудительная перерисовка
+        titleSlider.classList.add('text-animation'); // Добавляем класс анимации
+
+        // Убираем класс 'active' у всех кнопок
+        sliderButtons.forEach(btn => btn.classList.remove('active'));
+        // Добавляем класс 'active' только выбранной кнопке
+        sliderButtons[index].classList.add('active');
+
         currentIndex = index;
     }
 
@@ -21,7 +33,6 @@ document.addEventListener("DOMContentLoaded", function () {
             showImage(index);
         });
     });
-
     // Показываем первое изображение при загрузке страницы
     showImage(currentIndex);
 });
